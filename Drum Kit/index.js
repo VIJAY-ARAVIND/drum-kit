b46e926907296c0b556d3981a1e1b6a84b1f3f1c
VIJAY ARAVIND ADIMADHYAM <36584896+VIJAY-ARAVIND@users.noreply.github.com>
2022-10-01 22:23:02 +0530

    Add files via upload

---
+++ Drum Kit/index.js
@@ -0,0 +1,53 @@
+var lengthofvars = document.querySelectorAll(".drum").length;
+for (var i=0;i<lengthofvars;i++){
+
+
+document.querySelectorAll(".drum")[i].addEventListener("click",onClick);
+
+}
+
+function onClick(){
+    var buttoninnerHTML = this.innerHTML;
+    sound(buttoninnerHTML);
+    buttonAnimation(buttoninnerHTML)
+
+    
+}
+
+document.addEventListener("keypress", function(event){ sound(event.key);buttonAnimation(event.key)});
+
+function sound(key){
+    switch (key) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play()
+        case "a":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play()
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play()
+        case "d":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play()
+        case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play()
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play()
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play()
+    }
+}
+
+function buttonAnimation(currentkey) {
+
+    var active = document.querySelector("."+currentkey);
+    active.classList.add("pressed");
+    setTimeout(function(){
+        active.classList.remove("pressed")
+    },100)
+
+}
